a443456b919d82245b0b75dce7f8809e0404749c
Raymond Gaius Yidawi <rayidatech@gmail.com>
2023-10-13 20:46:26 +0100

    JavaScript code added

---
+++ Section-02/my.js
@@ -0,0 +1,21 @@
+
+let errorparagraph = document.getElementById('error');
+
+function purchase(){
+    console.log("buttons clicked")
+    errorparagraph.textContent = 'Something went wrong'
+}
+
+
+
+
+
+// function showErrorMessage() {
+//     var errorMessage = document.getElementById('error-message');
+//     errorMessage.style.display = 'block';
+// }
+
+
+// function show() {
+//     var show = document.getElementById('error');
+//     show.style.display = 'block';}
